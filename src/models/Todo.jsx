64bf209b152bfef0beccc2dad6c9c928294686b8
scrapjo <darklord85@gmail.com>
2025-04-02 22:26:38 +0200

    tried to set up mongo db vercel, got stuck

--- src/models/Todo.jsx
+++ src/models/Todo.jsx
@@ -1,9 +1,14 @@
 import { Schema, model } from "mongoose";
 
-const todoSchema = new Schema({
+const mongoose = require('mongoose');
+
+
+
+const todoSchema = new mongoose.Schema({
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
 
-export const Todo = model("Todo", todoSchema);
+export const Todo = mongoose.models.Todo || mongoose.model('Todo, todoSchema')
+
