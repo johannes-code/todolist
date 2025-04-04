2156792fff65a41a38ff32057b18cace21f5beaa
scrapjo <darklord85@gmail.com>
2025-04-04 23:50:49 +0200

    still not working

--- src/middelware.js
+++ src/middelware.js
@@ -1,22 +1,18 @@
-import { clerkMiddleware, auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware((auth, req) => {
-    publicRoutes: ['/']
-    // This function will be called with the auth and request objects
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
-    if (!auth().userId && !req.nextUrl.pathname !== '/') { // Example: Redirect if not signed in and not on the homepage
-        return NextResponse.redirect(new URL('/sign-in', req.url)); // Assuming you have a /sign-in route
-      }
-
-      return NextResponse.next();
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    ],
-};
+  ],
+}
