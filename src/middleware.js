a53d01c3b2cd559adc0e8ba227950274ee5664a1
scrapjo <darklord85@gmail.com>
2025-04-07 09:26:29 +0200

    clerk is in the house

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+export default clerkMiddleware(async ( auth, req) => {
+  const publicRoutes = ['/sign-in', '/sign-up'];
+  if (!publicRoutes.includes(req.nextUrl.pathname)) {
+    await auth.protect();
   }
-})
+});
+  
+
 
 export const config = {
   matcher: [
