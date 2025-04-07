79207187b112255c89cae0eb82f31e23458c3cf8
scrapjo <darklord85@gmail.com>
2025-04-07 23:18:34 +0200

    still strugggles with the sign up button not behaving as wanted.

--- src/middleware.js
+++ src/middleware.js
@@ -8,15 +8,18 @@ export default clerkMiddleware(async (auth, request) => {
 
   if (!isPublicRoute) {
     const user = auth.user;
-    if (!user) {
-      // Redirect unauthenticated users to /sign-in
+        
+      if (!user && request.nextUrl.pathname === '/sign-up'){
+        return NextResponse.redirect(new URL('/sign-up', request.url));
+      }
+      if (!user)
       return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
+    
   }
 
   return NextResponse.next();
 });
-
+ 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
