207d0c56a5e38e760cfd3276e20bef687c6fe7ef
scrapjo <darklord85@gmail.com>
2025-04-10 22:16:19 +0200

    got to the add todo page

--- src/models/User.jsx
+++ src/models/User.jsx
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true },
-  encryptedDataKey: { type: string },
+  encryptedDataKey: { type: String },
 });
 
-const User = mongoose.models.User || mongoose.model("User, UserSchema");
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
