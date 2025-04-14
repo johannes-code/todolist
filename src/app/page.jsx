2cc9d93e5d33394acd400fd0fc9b7082d3aa9381
johannes-code <johannes24sfj@gmail.com>
2025-04-14 14:31:19 +0200

    *

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,5 @@ import HomeClient from "@/app/home-client";
 import HomeClientWrapper from "@/components/HomeClientWrapper";
 
 export default function Home() {
-  return (
-  
-  <HomeClient />
-  );
+  return <HomeClient />;
 }
