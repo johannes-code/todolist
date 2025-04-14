6ae23abbcffbbf6e4b5b03415615c4013bffb3e6
johannes-code <johannes24sfj@gmail.com>
2025-04-14 10:58:55 +0200

    made user webhook updated usermodel

---
+++ src/app/api/webhooks/users/route.js
@@ -0,0 +1,72 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import User from "@/models/user";
+
+const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+
+export async function POST(req) {
+  const payload = await req.text();
+  const svixId = (await headers()).get("svix-id");
+  const svixSignature = headers().get("svix-signature");
+
+  if (!svixId || !svixSignature || !webhookSecret) {
+    return new Response("Error occured -- no svix headers or web secret.", {
+      status: 400,
+    });
+  }
+
+  const wh = new Webhook(webhookSecret);
+  let evt;
+
+  try {
+    evt = wh.verify(payload, {
+      "svix-id": svixId,
+      "svix-signature": svixSignature,
+    });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured -- invalid signature", { status: 400 });
+  }
+
+  const eventType = evt?.type;
+
+  if (eventType === "user.created") {
+    const {
+      id,
+      email_adresses: email_addresses,
+      first_name,
+      last_name,
+    } = evt.data;
+
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+
+      const newUser = new User({
+        clerkId: id,
+        email: email_addresses[0]?.email_address,
+        firstName: first_name,
+        lastName: last_name,
+      });
+
+      await newUser.save();
+
+      console.log("User created in DB", id);
+      return NextResponse.json(
+        { message: "User created successfully" },
+        { status: 201 }
+      );
+    } catch (error) {
+      console.error("Error creating user in DB", error);
+      return NextResponse.json(
+        { error: "Failed to create user in DB" },
+        { status: 500 }
+      );
+    } finally {
+      await mongoose.disconnect();
+    }
+  }
+
+  return new Response(`Webhook recieved: ${eventType}`, { status: 200 });
+}
--- src/models/User.jsx
+++ src/models/User.jsx
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
 
-const userSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true },
-  encryptedDataKey: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: { type: String, required: true, unique: true },
+    encryptedDataKey: { type: String },
 
+    email: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true, unique: false },
+    lastName: { type: String, required: true, unique: false },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
