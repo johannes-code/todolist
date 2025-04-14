78b545377350c547cf6855daa96d077653c28d31
johannes-code <johannes24sfj@gmail.com>
2025-04-14 14:21:26 +0200

    corrected wrong imports

--- src/app/api/encryption/setup/route.js
+++ src/app/api/encryption/setup/route.js
@@ -1,9 +1,9 @@
 // src/app/api/encryption/setup/route.js
 
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { connectToDB } from "@/app/lib/db";
 import User from "@/models/User";
-import sodium, { initializeSodium } from "@/utils/encryption";
+import { sodium, initializeSodium } from "@/utils/encryption";
 
 const SERVER_SECRET_KEY = process.env.SERVER_ENCRYPTION_SECRET;
 
