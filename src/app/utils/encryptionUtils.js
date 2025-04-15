e1cb6ae4c2bc4d8ee5f53d2cfb7920408a286603
johannes-code <johannes24sfj@gmail.com>
2025-04-15 10:41:59 +0200

    restarted working on the encryption

---
+++ src/app/utils/encryptionUtils.js
@@ -0,0 +1,53 @@
+async function generateEncryptionKey() {
+    return await window.crypto.subtle.generateKey(
+      {
+        name: "AES-GCM",
+        length: 256, 
+      },
+      true, 
+      ["encrypt", "decrypt"] 
+    );
+  }
+
+async function exportKey(key) {
+    return await window.crypto.subtle.exportKey("raw", key);
+}
+
+async function importKey(exportedKey) {
+    return await window.crypto.subtle.importKey(
+        "jwk",
+        exportedKey
+        {
+            name: "AES-GCM",
+            length: 256,
+        },
+        true,
+        ["encrypt", "decrypt"]
+    )
+}
+
+async function encryptData(key, data) {
+    const iv = window.crypto.getRandomValues(new Uint8Array(12)); // Initialization vector
+    const encodedData = new TextEncoder().encode(data);
+    
+    const ciphertext = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        key,
+        encodedData
+    );
+}
+async function decryptData(key, iv, ciphertext) {
+    const decryptedData = await window.crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        key,
+        ciphertext
+    );
+    
+    return new TextDecoder().decode(decryptedData);
+}
